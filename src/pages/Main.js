53f283674c8e9e2354d3dcd496e398b3eec22de9
Hangrykat <Hangrykat@users.noreply.github.com>
2020-11-27 16:48:15 +0100

    mainpage layout

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,12 +7,12 @@ const MainPage = () => {
         <div className="nav-wrapper">
           <div class="logo">[ColorTest]</div>
           <span></span>
-          <span>About Color Blindness</span>
+          <span>ABOUT COLOR BLINDNESS</span>
           <span>
-            <Link to="test">Color Blind Test </Link>
+            <Link to="test">COLOR BLIND TEST </Link>
           </span>
 
-          <span>Contact us</span>
+          <span>CONTACT US</span>
         </div>
       </div>
       <div className="main"></div>
@@ -24,6 +24,25 @@ const MainPage = () => {
       <div className="content-section">
         <div className="content-wrapper">
           <div className="section-header"> What is color blindness?</div>
+          <div className="section-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida, enim vel lobortis pretium, lacus ligula volutpat metus, vel feugiat massa nisl id ex. Praesent in lacus auctor, dapibus mi accumsan, facilisis leo. Donec egestas nulla leo, eu porta orci venenatis ac. Duis mollis metus in massa pulvinar, a ullamcorper ex congue. Fusce placerat nunc sapien, fringilla dictum quam congue dapibus. Donec sem metus, vulputate vel venenatis ut, tristique id sapien. Aenean quis viverra ex.</div>
+        </div>
+      </div>
+      <div className="content-section">
+        <div className="content-wrapper">
+          <div className="section-header"> Take a quick test?</div>
+          <div className="section-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque gravida, enim vel lobortis pretium, lacus ligula volutpat metus, vel feugiat massa nisl id ex. Praesent in lacus auctor, dapibus mi accumsan, facilisis leo. Donec egestas nulla leo, eu porta orci venenatis ac. Duis mollis metus in massa pulvinar, a ullamcorper ex congue. Fusce placerat nunc sapien, fringilla dictum quam congue dapibus. Donec sem metus, vulputate vel venenatis ut, tristique id sapien. Aenean quis viverra ex.</div>
+        </div>
+      </div>
+      <div className="content-section">
+        <div className="content-wrapper">
+          <div className="section-header"> Contact us</div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="content-wrapper">
+          <div className="footer-description">
+          © 2020 ColorTest Private Limited All Rights Reserved
+          </div>
         </div>
       </div>
     </>
