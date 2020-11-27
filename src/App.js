3ee0fa1f9c3f9413c5bd605b5acfbe1f90e7ebe6
Hsueh Yen Neo <neo.hsuehyen@gmail.com>
2020-11-27 15:08:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import header from "./assets/header.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <div className="nav-wrapper">
+          <div class="logo">[ColorTest]</div>
+          <span></span>
+          <span>About Color Blindness</span>
+          <span>Color Blind Test</span>
+          <span>Contact us</span>
+        </div>
+      </div>
+      <div className="main"></div>
+      <div className="header-container">
+        <div className="content-wrapper">
+          <img src={header} alt="iris of eye" />
+        </div>
+      </div>
+      <div className="content-section">
+        <div className="content-wrapper">
+          <div className="section-header"> What is color blindness?</div></div>
+      </div>
     </div>
   );
 }
